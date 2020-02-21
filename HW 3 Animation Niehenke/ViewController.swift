9112923a17da75d9624378a9c71b8b66b2d5e37f
Adam Niehenke <a.niehenke@setonhill.edu>
2020-02-20 19:28:15 -0500

    Initial Commit

---
+++ HW 3 Animation Niehenke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW 3 Animation Niehenke
+//
+//  Created by Adam Niehenke on 2/20/20.
+//  Copyright © 2020 Adam Niehenke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW 3 Animation NiehenkeTests/HW_3_Animation_NiehenkeTests.swift
@@ -0,0 +1,34 @@
+//
+//  HW_3_Animation_NiehenkeTests.swift
+//  HW 3 Animation NiehenkeTests
+//
+//  Created by Adam Niehenke on 2/20/20.
+//  Copyright © 2020 Adam Niehenke. All rights reserved.
+//
+
+import XCTest
+@testable import HW_3_Animation_Niehenke
+
+class HW_3_Animation_NiehenkeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
